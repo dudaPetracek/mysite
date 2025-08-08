2c0341445e436e9dc3fe934ddeefc1b065006eb5
Duda Petracek <157414482+dudaPetracek@users.noreply.github.com>
2025-08-08 10:25:06 -0300

    Update urls.py

--- mysite_project/core/urls.py
+++ mysite_project/core/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('blog.urls')),
+    path('', include('blog.urls')), 
 ]
+
+
 
 
 
